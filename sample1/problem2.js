16c690a0a503ab6820ba861886f43f76fc106922
Snehal-arsad <snehalarsad52@gmail.com>
2022-10-13 13:39:38 +0530

    This is my first assignment folder

---
+++ sample1/problem2.js
@@ -0,0 +1,26 @@
+let budjet = 1500;
+
+if (budjet >= 5000)
+{
+  console.log("Dominos");
+}
+else if (budjet>=2000)
+{
+  console.log("dhaba");
+}
+else if (budjet >= 500)
+{
+  console.log("vada pav vendor");
+}
+else if (budjet >= 100)
+{
+  console.log("Tea party");
+}
+else if (budjet >= 10)
+{
+  console.log("Eaclairs");
+}
+else 
+{
+  console.log("no party");
+}
